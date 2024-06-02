b4f1bc84279961c18c8269139be8ec6cd083ecf7
JunaiCode <juanpalta24@hotmail.com>
2024-06-02 11:14:56 -0500

    formData work correctly, add blob to download file

--- src/Components/DropZone.js
+++ src/Components/DropZone.js
@@ -3,7 +3,7 @@ import { useDropzone } from 'react-dropzone';
 import { MdOutlineCancel } from "react-icons/md";
 import './MyDropzone.css';
 
-function MyDropzone({ mode }) {
+function MyDropzone({ mode, keyValue }) {
   const [selectedFile, setSelectedFile] = useState(null);
 
   const onDrop = useCallback(acceptedFiles => {
@@ -11,6 +11,42 @@ function MyDropzone({ mode }) {
       setSelectedFile(acceptedFiles[0]);
     }
   }, []);
+
+  const handleClick = () => {
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+      formData.append('key', keyValue);
+
+      const url = mode === "encrypt" 
+        ? 'http://localhost:8080/encryptor/encrypt'
+        : 'http://localhost:8080/encryptor/decrypt';
+
+      fetch(url, {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.blob();
+      })
+      .then(blob => {
+        const downloadUrl = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = downloadUrl;
+        a.download = selectedFile.name; // Puedes cambiar el nombre del archivo si es necesario
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+        window.URL.revokeObjectURL(downloadUrl);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+    }
+  };
 
   const removeFile = () => {
     setSelectedFile(null);
@@ -27,18 +63,20 @@ function MyDropzone({ mode }) {
       <div {...getRootProps({ className: 'dropzone' })}>
         <input {...getInputProps()} />
         {selectedFile && (
-        <div className="file-info">
-          <p>Selected file: {selectedFile.name}</p>
-          <MdOutlineCancel className="remove-button" onClick={removeFile} />
-        </div>
-      )}
+          <div className="file-info">
+            <p>Selected file: {selectedFile.name}</p>
+            <MdOutlineCancel className="remove-button" onClick={removeFile} />
+          </div>
+        )}
         {isDragActive && !selectedFile ? (
           <p>Drop the file here...</p>
         ) : (
           !selectedFile && <p>Drag 'n' drop a file here, or click to select a file</p>
         )}
       </div>
-      <button className="button">{mode==="encrypt"?"Encrypt":"Decrypt"}</button>
+      <button onClick={handleClick} className="button">
+        {mode === "encrypt" ? "Encrypt" : "Decrypt"}
+      </button>
     </div>
   );
 }
